4ba5a2c562d73872c85c6b9ecd038f40729d8845
Takeru Ohta <phjgt308@gmail.com>
2026-01-26 08:46:58 +0900

    feat: add argument parsing with noargs library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> noargs::Result<()> {
+    let mut args = noargs::raw_args();
+
+    args.metadata_mut().app_name = env!("CARGO_PKG_NAME");
+    args.metadata_mut().app_description = env!("CARGO_PKG_DESCRIPTION");
+
+    if noargs::VERSION_FLAG.take(&mut args).is_present() {
+        println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+    noargs::HELP_FLAG.take_help(&mut args);
+
+    let ext = noargs::flag("ext")
+        .short('x')
+        .doc("Enable extended subcommands")
+        .take(&mut args)
+        .is_present();
+
+    if flag {
+        //    let _ = ruuk::command_list::run(&mut args)? || ruuk::command_view::run(&mut args)?;
+    } else {
+        // run main command
+    }
+
+    if let Some(help) = args.finish()? {
+        print!("{help}");
+    }
+    return Ok(());
 }
