71eeb1a6151c5e4f29b25e085ad6ee02688f10df
Takeru Ohta <phjgt308@gmail.com>
2026-01-28 23:15:31 +0900

    test: add test for function with generic trait bounds

--- src/format_item.rs
+++ src/format_item.rs
@@ -283,6 +283,106 @@ impl<'a, W: std::io::Write> FunctionFormatter<'a, W> {
 mod tests {
     use super::*;
 
-    // todo: test
-    //  {"sig":{"inputs":[["key",{"generic":"K"}]],"output":{"resolved_path":{"path":"Result","id":406,"args":{"angle_bracketed":{"args":[{"type":{"resolved_path":{"path":"String","id":447,"args":null}}},{"type":{"resolved_path":{"path":"std::env::VarError","id":628,"args":null}}}],"constraints":[]}}}},"is_c_variadic":false},"generics":{"params":[{"name":"K","kind":{"type":{"bounds":[{"trait_bound":{"trait":{"path":"AsRef","id":629,"args":{"angle_bracketed":{"args":[{"type":{"resolved_path":{"path":"std::ffi::OsStr","id":630,"args":null}}}],"constraints":[]}}},"generic_params":[],"modifier":"none"}},{"trait_bound":{"trait":{"path":"AsRef","id":629,"args":{"angle_bracketed":{"args":[{"type":{"primitive":"str"}}],"constraints":[]}}},"generic_params":[],"modifier":"none"}}],"default":null,"is_synthetic":false}}}],"where_predicates":[]},"header":{"is_const":false,"is_unsafe":false,"is_async":false,"abi":"Rust"},"has_body":true}
+    #[test]
+    fn format_function_with_generic_trait_bounds() -> crate::Result<()> {
+        let doc = empty_doc();
+        let json_str = r#"{
+        "name": "var",
+        "visibility": "public",
+        "inner": {
+            "function": {
+                "sig": {
+                    "inputs": [["key", {"generic": "K"}]],
+                    "output": {
+                        "resolved_path": {
+                            "path": "Result",
+                            "id": 406,
+                            "args": {
+                                "angle_bracketed": {
+                                    "args": [
+                                        {"type": {"resolved_path": {"path": "String", "id": 447, "args": null}}},
+                                        {"type": {"resolved_path": {"path": "std::env::VarError", "id": 628, "args": null}}}
+                                    ],
+                                    "constraints": []
+                                }
+                            }
+                        }
+                    },
+                    "is_c_variadic": false,
+                    "header": {
+                        "is_const": false,
+                        "is_unsafe": false,
+                        "is_async": false,
+                        "abi": "Rust"
+                    }
+                },
+                "generics": {
+                    "params": [{
+                        "name": "K",
+                        "kind": {
+                            "type": {
+                                "bounds": [
+                                    {
+                                        "trait_bound": {
+                                            "trait": {
+                                                "path": "AsRef",
+                                                "id": 629,
+                                                "args": {
+                                                    "angle_bracketed": {
+                                                        "args": [{"type": {"resolved_path": {"path": "std::ffi::OsStr", "id": 630, "args": null}}}],
+                                                        "constraints": []
+                                                    }
+                                                }
+                                            },
+                                            "generic_params": [],
+                                            "modifier": "none"
+                                        }
+                                    },
+                                    {
+                                        "trait_bound": {
+                                            "trait": {
+                                                "path": "AsRef",
+                                                "id": 629,
+                                                "args": {
+                                                    "angle_bracketed": {
+                                                        "args": [{"type": {"primitive": "str"}}],
+                                                        "constraints": []
+                                                    }
+                                                }
+                                            },
+                                            "generic_params": [],
+                                            "modifier": "none"
+                                        }
+                                    }
+                                ],
+                                "default": null,
+                                "is_synthetic": false
+                            }
+                        }
+                    }],
+                    "where_predicates": []
+                },
+                "has_body": true
+            }
+        },
+        "docs": null,
+        "deprecation": null
+    }"#;
+
+        let raw_json = nojson::RawJson::parse(json_str)?;
+        let item = crate::doc::Item::from_json(&doc.json, raw_json.value(), 0)?;
+        let formatted = format_function_to_string(&doc, &item)?;
+
+        assert_eq!(
+            formatted,
+            "fn var<K: AsRef<std::ffi::OsStr> + AsRef<str>>(key: K) -> Result<String, std::env::VarError>"
+        );
+
+        Ok(())
+    }
+
+    fn empty_doc() -> crate::doc::CrateDoc {
+        let text = r#"{"root": 0, "index": {"0": {"name": "test", "visibility": "public", "inner": {"module": {"items": []}}, "docs": null, "deprecation": null}}}"#;
+        crate::doc::CrateDoc::parse(std::path::PathBuf::from(""), text).expect("bug")
+    }
 }
