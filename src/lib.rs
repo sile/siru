56c29502dfc92dbf62d9a6228a548f39a9222aac
Takeru Ohta <phjgt308@gmail.com>
2026-01-29 18:34:55 +0900

    feat: add span tracking to JSON parse errors

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub enum Error {
     Io(std::io::Error),
     Json {
         error: nojson::JsonParseError,
+        span: Option<std::ops::Range<usize>>,
         text: Option<String>,
         backtrace: std::backtrace::Backtrace,
     },
@@ -25,11 +26,30 @@ impl Error {
         match self {
             Error::Json {
                 error,
+                span,
                 text: None,
                 backtrace,
             } => Error::Json {
                 error,
+                span,
                 text: Some(text.into()),
+                backtrace,
+            },
+            other => other,
+        }
+    }
+
+    pub fn set_json_span(self, value: nojson::RawJsonValue<'_, '_>) -> Self {
+        match self {
+            Error::Json {
+                error,
+                span: None,
+                text,
+                backtrace,
+            } => Error::Json {
+                error,
+                span: Some(value.position()..value.position() + value.as_raw_str().len()),
+                text,
                 backtrace,
             },
             other => other,
@@ -53,6 +73,7 @@ impl From<nojson::JsonParseError> for Error {
     fn from(error: nojson::JsonParseError) -> Self {
         Error::Json {
             error,
+            span: None,
             text: None,
             backtrace: std::backtrace::Backtrace::capture(),
         }
@@ -66,14 +87,28 @@ impl std::fmt::Display for Error {
             Error::Io(err) => write!(f, "IO error: {}", err),
             Error::Json {
                 error,
+                span,
                 text,
                 backtrace,
             } => {
                 if let Some(text) = text {
-                    write!(f, "{}", crate::json::format_parse_error(text, error))
+                    write!(f, "{}", crate::json::format_parse_error(text, error))?;
+
+                    // Show the range of text with span highlighting
+                    if let Some(span) = span {
+                        if span.start < text.len() && span.end <= text.len() {
+                            let error_text = &text[span.clone()];
+                            write!(
+                                f,
+                                "\n  at position {}..{}: \"{}\"",
+                                span.start, span.end, error_text
+                            )?;
+                        }
+                    }
                 } else {
-                    write!(f, "JSON parse error: {}", error)
-                }?;
+                    write!(f, "JSON parse error: {}", error)?;
+                }
+
                 if backtrace.status() == std::backtrace::BacktraceStatus::Captured {
                     write!(f, "\n{}", backtrace)?;
                 }
